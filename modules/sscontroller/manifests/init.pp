691c53df0550b65a1f80b1a4012c7fcd93d469d8
root <root@ss-build-6189.ccg21.dev.paypalcorp.com>
2016-08-14 12:34:57 -0700

    added swiftstack download from swift

--- modules/sscontroller/manifests/init.pp
+++ modules/sscontroller/manifests/init.pp
@@ -1,9 +1,4 @@
 class sscontroller {
-  file { '/tmp/testfile':
-	ensure => file,
-	source => 'puppet:///modules/sscontroller/testfile',
-  }
-
   package { 'rng-tools':
     ensure => 'installed',
   }
@@ -13,6 +8,17 @@ class sscontroller {
     path => '/usr/sbin',
     require => Package['rng-tools'],
   }
+
+  exec {'swiftstack-files':
+    creates => '/tmp/swiftstack',
+    command => 'mkdir /tmp/swiftstack; cd /tmp/swiftstack;
+	for f in setup-swiftstack-2.24.0.2_ebay_2014-06-28.sh SwiftStackController-4.1.0.tar.gz; do
+	   wget http://os-swift-proxy.vip.lvs03.paypalc3.com:8080/v1/KEY_0a20f3fbeaf2450a809c107ffa687baf/swiftstack/swiftstack/$f;
+	done',
+    path => ['/bin', '/usr/bin'],
+  }
+
+
 }
 
 
