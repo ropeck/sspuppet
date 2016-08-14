6616bac84e240800ad766390526b9caf750b63c7
root <root@ss-build-6189.ccg21.dev.paypalcorp.com>
2016-08-14 11:40:16 -0700

    added rndg setup

--- modules/sscontroller/manifests/init.pp
+++ modules/sscontroller/manifests/init.pp
@@ -3,6 +3,16 @@ class sscontroller {
 	ensure => file,
 	source => 'puppet:///modules/sscontroller/testfile',
   }
+
+  package { 'rng-tools':
+    ensure => 'installed',
+  }
+
+  exec {'rng-setup':
+    command => 'rngd -r /dev/urandom',
+    path => '/usr/sbin',
+    require => Package['rng-tools'],
+  }
 }
 
 
