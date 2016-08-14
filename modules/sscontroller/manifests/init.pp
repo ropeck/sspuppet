7700458c1337f9b8cf4e98cf1cdada7f2c7c5f57
root <root@ss-build-6189.ccg21.dev.paypalcorp.com>
2016-08-14 13:21:59 -0700

    more

--- modules/sscontroller/manifests/init.pp
+++ modules/sscontroller/manifests/init.pp
@@ -14,11 +14,22 @@ class sscontroller {
     command => 'mkdir /tmp/swiftstack; cd /tmp/swiftstack;
 	for f in setup-swiftstack-2.24.0.2_ebay_2014-06-28.sh SwiftStackController-4.1.0.tar.gz; do
 	   wget http://os-swift-proxy.vip.lvs03.paypalc3.com:8080/v1/KEY_0a20f3fbeaf2450a809c107ffa687baf/swiftstack/swiftstack/$f;
-	done',
+	done;
+	chmod +x *sh',
     path => ['/bin', '/usr/bin'],
   }
 
 
+  group { 'swiftstack':
+    gid => 1001,
+  }
+
+  user { 'swiftstack':
+    ensure     => 'present',
+    managehome => true,
+    uid => 1001,
+    groups => 'swiftstack',
+  }
 }
 
 
